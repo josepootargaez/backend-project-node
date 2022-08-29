2d5a523acfd940d1dd3c34b6cfcdf4d5e3723282
Jose-L-Poot <jose.poot@gulf.mx>
2022-08-29 16:34:37 -0500

    add cors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const app = express();
 const port = process.env.PORT_SERVER;
 const router = require('./routes/index')
 const  morgan = require('morgan')
+const cors = require('cors');
+
 //middlewares
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(morgan('dev'))
